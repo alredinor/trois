1ff2b1835ccefdc4a1833a416f57d81ca5538a27
charley <charleybossard@gmail.com>
2020-01-23 11:28:53 +0100

    CompteRepository

--- src/main/java/repository/CompteRepository.java
+++ src/main/java/repository/CompteRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import model.Compte;
 
-public interface CompteRepository extends JpaRepository<Compte, Integer> {
+public interface CompteRepository extends JpaRepository<Compte, Long> {
 	
 }
