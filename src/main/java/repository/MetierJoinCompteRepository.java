15dd412ccde412aa2434fe19ee5bb398ff4de948
Nico <touboulnicolas@gmail.com>
2020-01-23 10:43:42 +0100

    metierjoincompte nico

--- src/main/java/repository/MetierJoinCompteRepository.java
+++ src/main/java/repository/MetierJoinCompteRepository.java
@@ -1,9 +1,18 @@
 package repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import model.Compte;
+import model.Metier;
 import model.MetierJoinCompte;
 
 public interface MetierJoinCompteRepository extends JpaRepository<MetierJoinCompte, Integer> {
 	
+	
+	Optional <MetierJoinCompte> findById (Integer id);
+	List <MetierJoinCompte> findByMetier(Metier metier);
+	List <MetierJoinCompte> findByCompte(Compte compte);
 }
