7f58a8a5728f0de2feab12dce30e65c4e8f1ee82
Hubert MOKET <hubertayiwa@gmail.com>
2020-01-23 13:39:16 +0100

    hubert

--- src/test/java/test/TestDetailRepository.java
+++ src/test/java/test/TestDetailRepository.java
@@ -1,4 +1,6 @@
-import static org.junit.Assert.*;
+package test;
+
+import static org.junit.Assert.assertFalse;
 
 import java.util.Optional;
 
@@ -9,20 +11,27 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import model.Demande;
-import repository.DemandeRepository;
+import model.Detail;
+import model.DetailPK;
+import model.Service;
+import repository.DetailRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/application-context.xml"})
-public class TestDemandeRepository {
+public class TestDetailRepository {
 	
 	@Autowired
-	private DemandeRepository demandeRepository;
+	private DetailRepository detailRepository;
 
 	@Test
 	public void test() {
 		
-		Optional<Demande> opt=demandeRepository.findById(1L);
-		assertFalse(opt.isPresent());
+//		Demande d=new Demande();
+//		Service s=new Service();
+//		DetailPK key=new DetailPK(d, s);
+//		Optional<Detail> opt=detailRepository.findById(key);
+//		assertFalse(opt.isPresent());
+		
 	}
 
 }
