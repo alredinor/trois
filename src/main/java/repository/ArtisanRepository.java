b0a369bb414a6ec9efec3943cbab3f084225d5c6
alban <alban.delouvee@gmail.com>
2020-01-23 10:43:34 +0100

    alban

--- src/main/java/repository/ArtisanRepository.java
+++ src/main/java/repository/ArtisanRepository.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Metier;
-import model.Service;
+import model.Adresse;
 import model.Artisan;
+
 
 public interface ArtisanRepository extends JpaRepository<Artisan, Long> {
 	
 	
-	 List<Metier> findAllMetier();
-	 List<Service> findAllService();
-	 List<Service> findAllDemandes();
+	List<Artisan> findByLogin(String login);
+	List<Artisan> findByAdresse(Adresse adresse);
+	
+
+	
 }
