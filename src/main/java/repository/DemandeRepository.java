96badf204d953b65b0bd35d28e2ac158f46cdff8
alban <alban.delouvee@gmail.com>
2020-01-23 15:24:08 +0100

    alban

--- src/main/java/repository/DemandeRepository.java
+++ src/main/java/repository/DemandeRepository.java
@@ -1,14 +1,23 @@
 package repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import model.Compte;
 import model.Demande;
-import java.lang.Long;
-import java.util.List;
+import model.Metier;
+import model.Service;
+import model.StatutDemande;
 
 public interface DemandeRepository extends JpaRepository<Demande, Long>{
 	
-	List<Demande> findByIdDemande(Long iddemande);
+	List<Demande> findByIdDemande(Long idDemande);
+	List<Demande> findByArtisan(Compte idArtisan);
+	List<Demande> findByClient(Compte idClient);
+	List<Demande> findByService(Service idService);
+	List<Demande> findByMetier(Metier idMetier);
+	List<Demande> findByStatut(StatutDemande statut);
 	
 
 }
