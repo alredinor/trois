65d7690911989ccf3fa3369a942dfac2f0c99bf2
alban <alban.delouvee@gmail.com>
2020-01-23 11:38:03 +0100

    alban

--- src/main/java/repository/ClientRepository.java
+++ src/main/java/repository/ClientRepository.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import model.Artisan;
+import model.Adresse;
 import model.Client;
-import model.Metier;
-import model.Service;
+
 
 public interface ClientRepository extends JpaRepository<Client, Long>{
 	
 	
-	List<Metier> findAllMetier(String nom);
-	List<Service> findAllService(String nom);
-	List<Artisan> findAllArtisan(String nom);
+	List<Client> findByIdCompte(Long idCompte);
+	List<Client> findByAdresse(Adresse adresse);
+	List<Client> findByLogin(String login);
+	List<Client> findByEmail(String email);
 }
--- src/main/java/repository/ServiceRepository.java
+++ src/main/java/repository/ServiceRepository.java
@@ -1,9 +1,16 @@
 package repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import model.Metier;
 import model.Service;
 
 public interface ServiceRepository extends JpaRepository<Service, Long> {
 	
+	Optional<Service> findByIdService(Long idService);
+	List<Service> findByNomService(String nomService);
+	List<Service> findByMetier(Metier idMetier);
 }
